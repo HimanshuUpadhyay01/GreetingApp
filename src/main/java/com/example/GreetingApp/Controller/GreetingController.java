86bb569953f48648e50765a328cf7f948a297c51
HimanshuUpadhyay01 <hu7131@gmail.com>
2025-03-06 00:48:04 +0530

    greeting app with database save

---
+++ src/main/java/com/example/GreetingApp/Controller/GreetingController.java
@@ -0,0 +1,39 @@
+package com.example.GreetingApp.Controller;
+import com.example.GreetingApp.Service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController {
+    @GetMapping
+    public String getGreeting() {
+        return "Hello, this is a GET request!";
+    }
+
+    private final GreetingService greetingService;
+    @Autowired
+    public GreetingController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    @GetMapping("/service")
+    public String getGreetingServiceLayer() {
+        return GreetingService.getGreetingMessage();
+    }
+
+    @GetMapping("/personalized")
+    public String getGreeting(@RequestParam(required = false) String firstName,
+                              @RequestParam(required = false) String lastName) {
+        return greetingService.getGreetingMessage(firstName, lastName);
+    }
+    @GetMapping("/UC4")
+    public String greet(@RequestParam(value = "firstName", required = false) String firstName,
+                        @RequestParam(value = "lastName", required = false) String lastName) {
+        String message = greetingService.getGreetingMessage(firstName, lastName);
+        greetingService.saveGreetingMessage(message);
+        return "{\"message\": \"" + message + "\"}";
+    }
+
+
+}
