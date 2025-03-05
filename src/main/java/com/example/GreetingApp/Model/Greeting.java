14e9dde32550bae38b7a8d9b6d0d3dab79166856
HimanshuUpadhyay01 <hu7131@gmail.com>
2025-03-06 01:02:42 +0530

    greeting app with deleteGreeting

---
+++ src/main/java/com/example/GreetingApp/Model/Greeting.java
@@ -0,0 +1,42 @@
+package com.example.GreetingApp.Model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Greeting {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String message;
+
+    // ✅ Default constructor (Required by JPA)
+    public Greeting() {
+    }
+
+    public Greeting(String message, Long id) {
+        this.message = message;
+        this.id = id;
+    }
+
+    public Greeting(String message) {
+        this.message = message;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
