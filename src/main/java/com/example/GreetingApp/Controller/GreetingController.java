1203f3e8a812bf7fc549de48acce8f18599fa62e
HimanshuUpadhyay01 <hu7131@gmail.com>
2025-03-06 01:00:26 +0530

    greeting app with updateGreeting

---
+++ src/main/java/com/example/GreetingApp/Controller/GreetingController.java
@@ -0,0 +1,66 @@
+package com.example.GreetingApp.Controller;
+import com.example.GreetingApp.Model.Greeting;
+import com.example.GreetingApp.Service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController {
+    @GetMapping
+    public String getGreeting() {
+        return "Hello, this is a GET request!";
+    }
+
+    private final GreetingService greetingService;
+    @Autowired
+    public GreetingController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    @GetMapping("/service")
+    public String getGreetingServiceLayer() {
+        return GreetingService.getGreetingMessage();
+    }
+
+    @GetMapping("/personalized")
+    public String getGreeting(@RequestParam(required = false) String firstName,
+                              @RequestParam(required = false) String lastName) {
+        return greetingService.getGreetingMessage(firstName, lastName);
+    }
+
+    @GetMapping("/UC4")
+    public String greet(@RequestParam(value = "firstName", required = false) String firstName,
+                        @RequestParam(value = "lastName", required = false) String lastName) {
+        String message = greetingService.getGreetingMessage(firstName, lastName);
+        greetingService.saveGreetingMessage(message);
+        return "{\"message\": \"" + message + "\"}";
+    }
+
+    @GetMapping("/UC5/{id}")
+    public ResponseEntity<?> getGreetingById(@PathVariable Long id) {
+        Optional<Greeting> greeting = greetingService.getGreetingById(id);
+        if (greeting.isPresent()) {
+            return ResponseEntity.ok(greeting.get());
+        } else {
+            return ResponseEntity.status(404)
+                    .body("{\"error\": \"Greeting not found with ID: " + id + "\"}");
+        }
+    }
+
+    @GetMapping("/all")
+    public List<Greeting> getAllGreetings() {
+        return greetingService.getAllGreetings();
+    }
+
+    @PutMapping("/update/{id}")
+    public Greeting updateGreeting(@PathVariable Long id, @RequestBody Greeting updatedGreeting) {
+        return greetingService.updateGreetingMessage(id, updatedGreeting.getMessage());
+    }
+}
